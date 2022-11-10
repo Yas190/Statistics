0622d0c3ad7f01b6e4e2c0b0964392c4895258e9
Yasmin Pires <78330539+Yas190@users.noreply.github.com>
2022-11-09 22:47:14 -0300

    Update Analise_enem.R

--- Analise_enem.R
+++ Analise_enem.R
@@ -5,24 +5,11 @@ library(tidyverse)
 
 # Importando Base de Dados
 enem_2021 <- as_tibble(
-  read.csv2("C:\\Users\\yasmin.pires\\OneDrive - Iteris Consultoria e Software\\Documentos\\Pessoal\\R\\microdados_enem_2021\\DADOS\\MICRODADOS_ENEM_2021.csv")
+  read.csv("C:\\Users\\yasmin.pires\\OneDrive - Iteris Consultoria e Software\\Documentos\\Pessoal\\R\\Curso\\dados filtrados.csv")
 )
 
 # Selecionando Colunas de interesse
 enem_2021_range <- enem_2021 %>%
-  select(
-    NU_INSCRICAO,
-    TP_SEXO,
-    TP_COR_RACA,
-    TP_DEPENDENCIA_ADM_ESC,
-    starts_with("TP_PRESENCA_"),
-    Q006,
-    NU_NOTA_CN,
-    NU_NOTA_CH,
-    NU_NOTA_LC,
-    NU_NOTA_MT
-    ) %>%
-  # Alterando Tipos das Variáveis
   mutate(
     across(c(NU_NOTA_CN, NU_NOTA_CH, NU_NOTA_LC, NU_NOTA_MT, starts_with("TP_PRESENCA_")), as.numeric)
     ) %>%
@@ -46,16 +33,7 @@ enem_2021_range <-enem_2021_range %>%
     MEDIA_GERAL = rowMeans(select(enem_2021_range, starts_with("NU_NOTA_")))
     ) %>%
   filter(
-    !is.na(MEDIA_GERAL)
-    ) %>%
-  mutate(
-    TP_COR_RACA = recode(
-      TP_COR_RACA, "0"= "Nao Declarado", "1"= "Branca", "2"= "Preta", "3"= "Parda", "4"= "Amarela", "5"= "Indigena", "6" = "Nao Dispoe"
-      ),
-    TP_DEPENDENCIA_ADM_ESC = recode(
-      TP_DEPENDENCIA_ADM_ESC, "1" = "Federal", "2" = "Estadual", "3" = "Municipal", "4" = "Privada"
-      )
-  )
+    !is.na(MEDIA_GERAL))
 
 sexo_feminino_2021 <- sum(enem_2021_range$TP_SEXO == "F")
 sexo_masculino_2021 <- sum(enem_2021_range$TP_SEXO == "M")
@@ -80,3 +58,22 @@ media_faixa3_2021 <- sapply(
 media_faixa4_2021 <- sapply(
   enem_2021_range[enem_2021_range$Q006 == "M"|enem_2021_range$Q006 == "N"|enem_2021_range$Q006 == "O"|enem_2021_range$Q006 == "P",
                   "MEDIA_GERAL"], mean)
+
+media_federal_2021 <- sapply(
+  enem_2021_range[enem_2021_range$TP_DEPENDENCIA_ADM_ESC == "Federal",
+                  "MEDIA_GERAL"], mean)
+media_estadual_2021 <- sapply(
+  enem_2021_range[enem_2021_range$TP_DEPENDENCIA_ADM_ESC == "Estadual",
+                  "MEDIA_GERAL"], mean)
+media_municipal_2021 <- sapply(
+  enem_2021_range[enem_2021_range$TP_DEPENDENCIA_ADM_ESC == "Municipal",
+                  "MEDIA_GERAL"], mean)
+media_municipal_2021 <- sapply(
+  enem_2021_range[enem_2021_range$TP_DEPENDENCIA_ADM_ESC == "Privada",
+                  "MEDIA_GERAL"], mean)
+
+media_CN_2021 <- mean(enem_2021_range$NU_NOTA_CN)
+media_CH_2021 <- mean(enem_2021_range$NU_NOTA_CH)
+media_LC_2021 <- mean(enem_2021_range$NU_NOTA_LC)
+media_MT_2021 <- mean(enem_2021_range$NU_NOTA_MT)
+
